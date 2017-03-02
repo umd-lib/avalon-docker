925ec5339bbf934063a1b53c8ca40656c0df6c8f
Michael B. Klein <mbklein@gmail.com>
2017-03-02 09:45:39 -0600

    Update install.sh
    Fix typo in stderr output

--- resources/install.sh
+++ resources/install.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo "Retriving and installing LaunchDaemons and pf rules for Avalon/Docker virtual networking..."
+echo "Retrieving and installing LaunchDaemons and pf rules for Avalon/Docker virtual networking..."
 curl -s -o /Library/LaunchDaemons/org.avalonmediasystem.lo0.172.16.123.1.plist https://raw.githubusercontent.com/avalonmediasystem/avalon-docker/development/resources/org.avalonmediasystem.lo0.172.16.123.1.plist
 curl -s -o /Library/LaunchDaemons/org.avalonmediasystem.pf.rails80.plist https://raw.githubusercontent.com/avalonmediasystem/avalon-docker/development/resources/org.avalonmediasystem.pf.rails80.plist
 echo "rdr pass inet proto tcp from any to 172.16.123.1 port 80 -> 127.0.0.1 port 3000" > /etc/pf.anchors/rails80
