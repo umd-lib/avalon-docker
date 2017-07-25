3fc91155476fecd0b49f46a8c6f902df86b6ba52
Michael Klein <mbklein@gmail.com>
2017-07-25 16:04:13 -0500

    Update fcrepo to 4.7.3 with postgres

--- fedora/fedora-entrypoint.sh
+++ fedora/fedora-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 chown jetty:jetty /data
-export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.home=/data"
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.home=/data -Dfcrepo.postgresql.host=db -Dfcrepo.postgresql.port=5432 -Dfcrepo.postgresql.username=fedora -Dfcrepo.postgresql.password=${FEDORA_DB_PASSWORD} -Dfcrepo.modeshape.configuration=classpath:/config/jdbc-postgresql/repository.json"
 exec /docker-entrypoint.sh $@
