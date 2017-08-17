6539d57225448c162f180037762bab076c79fb86
Phuong Dinh <phuongdh@gmail.com>
2017-08-16 20:40:21 -0400

    Improve Avalon dev image

--- avalon/rails_init-dev.sh
+++ avalon/rails_init-dev.sh
@@ -9,8 +9,10 @@ cp config/controlled_vocabulary.yml.example config/controlled_vocabulary.yml
 
 BACKGROUND=yes QUEUE=* bundle exec rake resque:work
 BACKGROUND=yes bundle exec rake environment resque:scheduler
+rm -f tmp/pids/server.pid
 bundle exec rake db:migrate
 bundle exec rails server -b 0.0.0.0 -p 80 
+tail -f /dev/null
 
 # batch ingest cronjob wouldn't autorun without this
 #touch /var/spool/cron/crontabs/app
