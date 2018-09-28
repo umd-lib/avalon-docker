11fed7907ec36791930e872510cc565359429965
Mohamed Abdul Rasheed <mohideen@umd.edu>
2018-09-28 12:04:18 -0400

    LIBAVALON-62. Fcrepo image improvements.
    - Added repository.json file.
    - Made database connection configurabel at run time via ENV variables
    - Configured default ENV values to point to DB container in avalon stack
    
    https://issues.umd.edu/browse/LIBAVALON-62

--- fedora/fedora-entrypoint.sh
+++ fedora/fedora-entrypoint.sh
@@ -6,6 +6,7 @@ if [ -f /run/secrets/ap-secrets ]; then
 fi
 
 chown jetty:jetty /data
-export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.modeshape.configuration=classpath:/config/jdbc-postgresql/repository.json"
-export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.postgresql.username=fedora -Dfcrepo.postgresql.password=${FEDORA_DB_PASSWORD} -Dfcrepo.postgresql.host=db"
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.modeshape.configuration=${FEDORA_MODESHAPE_CONFIGURATION}"
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.postgresql.host=${FEDORA_DB_HOST} -Dfcrepo.postgresql.port=${FEDORA_DB_PORT} -Dfcrepo.postgresql.db_name=${FEDORA_DB_NAME}"
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.postgresql.username=${FEDORA_DB_USER} -Dfcrepo.postgresql.password=${FEDORA_DB_PASSWORD}"
 exec /docker-entrypoint.sh $@
