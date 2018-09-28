ecb4d4807aaaf0d9afcdcbcf70e2926c4463402e
Mohamed Abdul Rasheed <mohideen@umd.edu>
2018-09-28 12:22:38 -0400

    LIBAVALON-62. Make binary size limit for database storage configurable.
    Also, increased the default limit to 4MB.
    
    https://issues.umd.edu/browse/LIBAVALON-62

--- fedora/fedora-entrypoint.sh
+++ fedora/fedora-entrypoint.sh
@@ -9,4 +9,5 @@ chown jetty:jetty /data
 export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.modeshape.configuration=${FEDORA_MODESHAPE_CONFIGURATION}"
 export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.postgresql.host=${FEDORA_DB_HOST} -Dfcrepo.postgresql.port=${FEDORA_DB_PORT} -Dfcrepo.postgresql.db_name=${FEDORA_DB_NAME}"
 export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.postgresql.username=${FEDORA_DB_USER} -Dfcrepo.postgresql.password=${FEDORA_DB_PASSWORD}"
+export JAVA_OPTIONS="${JAVA_OPTIONS} -Dfcrepo.binary.minimum.size=${FEDORA_BINARY_MIN_SIZE}"
 exec /docker-entrypoint.sh $@
