4458ed62e94125774f93f2612ed974abb92e0424
Mohamed Abdul Rasheed <mohideen@umd.edu>
2026-01-21 20:20:45 -0500

    LIBAVALON-500. Cache auth request to reduce load on web server.
    https://umd-dit.atlassian.net/browse/LIBAVALON-500

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -3,11 +3,13 @@
 # Make config file from template
 [ -z "$AVALON_DOMAIN" ] && AVALON_DOMAIN="http://avalon"
 [ -z "$AVALON_STREAMING_PORT" ] && AVALON_STREAMING_PORT=80
+[ -z "$PROXY_CACHE_VALID_DURATION" ] && PROXY_CACHE_VALID_DURATION=3m
 export AVALON_DOMAIN
 export AVALON_STREAMING_PORT
 # UMD Customization
 export AVALON_STREAMING_BASE_URL
-envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT,$AVALON_STREAMING_BASE_URL' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+export PROXY_CACHE_VALID_DURATION
+envsubst '$AVALON_DOMAIN,$AVALON_STREAMING_PORT,$AVALON_STREAMING_BASE_URL,$PROXY_CACHE_VALID_DURATION' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 # End UMD Customization
 
 exec /usr/local/nginx/sbin/nginx
