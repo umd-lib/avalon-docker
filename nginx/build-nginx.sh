149ddd9f4492672546a3ee38c6a9c18b5e04cbbe
Mohamed Abdul Rasheed <mohideen@umd.edu>
2026-01-21 20:20:36 -0500

    LIBAVALON-500. Update nginx to 1.24.
    - Old nginx version does not support cache for auth subrequest.
    - Nginx 1.24 is latest version that is compatible with the latest version of kaltura vod module.
    
    https://umd-dit.atlassian.net/browse/LIBAVALON-500

--- nginx/build-nginx.sh
+++ nginx/build-nginx.sh
@@ -1,11 +1,46 @@
 #!/bin/sh
-cd /usr/src && apt-get source nginx && mv nginx-* nginx
-cd /usr/src && git clone https://github.com/kaltura/nginx-vod-module.git && cd nginx-vod-module && git checkout -b latest-tag $(git describe --tags)
+# UMD Customization
+# Build newer version of NGINX with nginx-vod-module
+set -e
+
+NGINX_VERSION=1.24.0
+
+cd /usr/src
+
+# nginx source
+curl -fsSLO https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+tar xzf nginx-${NGINX_VERSION}.tar.gz
+mv nginx-${NGINX_VERSION} nginx
+
+# nginx-vod-module (latest stable tag)
+git clone https://github.com/kaltura/nginx-vod-module.git
+cd nginx-vod-module
+git fetch --tags
+git checkout $(git describe --tags --abbrev=0)
+
+# build nginx
 cd /usr/src/nginx
-./configure --with-file-aio --with-cc-opt="-O3" --conf-path=/etc/nginx/nginx.conf \
-    --add-module=/usr/src/nginx-vod-module \
-    --with-http_auth_request_module --with-http_sub_module --with-http_ssl_module --with-debug \
-    --error-log-path=/dev/stderr --http-log-path=/dev/stdout
-make && make install
+
+./configure \
+  --prefix=/usr/local/nginx \
+  --conf-path=/etc/nginx/nginx.conf \
+  --with-file-aio \
+  --with-threads \
+  --with-http_ssl_module \
+  --with-http_auth_request_module \
+  --with-http_sub_module \
+  --with-cc-opt="-O3" \
+  --add-module=/usr/src/nginx-vod-module \
+  --with-debug \
+  --error-log-path=/dev/stderr \
+  --http-log-path=/dev/stdout
+
+make -j$(nproc)
+make install
+
+# cleanup
 cd /
-rm -rf /usr/src/*
+rm -rf /usr/src/nginx \
+       /usr/src/nginx-vod-module \
+       /usr/src/nginx-${NGINX_VERSION}.tar.gz
+# End UMD Customization
